77ced7f984c922dc5e4e86a703c661df72daf3e1
sushmithakamath <74297664+sushmithakamath@users.noreply.github.com>
2023-06-14 19:14:12 +0530

    edited menu streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
-import streamlit
-streamlit.title('My Parents New healthy Diner')
 streamlit.header('Breakfast Menu')
-streamlit.text('Blueberry Oatmeal')
-streamlit.text('Coffee')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
